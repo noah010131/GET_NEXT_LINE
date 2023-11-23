d77b2bc8dd44aedde78b25181758f7c646318489
Chanyoung Park <chanypar@made-f0Cr3s6.clusters.42paris.fr>
2023-11-23 17:19:24 +0100

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanypar <chanypar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 12:06:02 by chanypar          #+#    #+#             */
+/*   Updated: 2023/11/23 12:08:28 by chanypar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
---
+++ test_gnl.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_gnl.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanypar <chanypar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 12:14:50 by chanypar          #+#    #+#             */
+/*   Updated: 2023/11/23 13:17:51 by chanypar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+int	main(void)
+{
+	int	fd;
+	int	bytes_read;
+	char	buff[100];
+
+	fd = open("test.c", O_RDONLY);
+	if (fd < 0)
+		return (0);
+	bytes_read = read(fd, buff, sizeof(buff));
+	if (bytes_read < 0)
+		return (0);
+	write(1, buff, bytes_read);
+	close(fd);
+	return (0);
+}
